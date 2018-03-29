2660fcb8b27cb92faa04a5ca57aaa19dd046e929
Spr1ngbr0ther <whl5926768@qq.com>
2018-03-29 16:22:52 +0800

    修复ssh爆破插件小问题

--- vulscan/vuldb/crack_ssh.py
+++ vulscan/vuldb/crack_ssh.py
@@ -15,8 +15,9 @@ def get_plugin_info():
     }
     return plugin_info
 
-
 def check(ip, port, timeout):
+    success_dict = {}
+    # PASSWORD_DIC = ['123456', 'admin', 'test','123123']
     user_list = ['root', 'admin', 'oracle', 'weblogic']
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -27,8 +28,13 @@ def check(ip, port, timeout):
                 ssh.connect(ip, port, user, pass_, timeout=timeout, allow_agent = False, look_for_keys = False)
                 ssh.exec_command('whoami',timeout=timeout)
                 if pass_ == '': pass_ = "null"
-                return u"存在弱口令，账号：%s，密码：%s" % (user, pass_)
+                success_dict[user] = pass_
+                continue 
             except Exception, e:
                 if "Unable to connect" in e or "timed out" in e: return
             finally:
                 ssh.close()
+    return u"存在弱口令 %s" % str(success_dict).lstrip('{').rstrip('}')
+
+# if __name__ == "__main__":
+#     check('',22,30)
